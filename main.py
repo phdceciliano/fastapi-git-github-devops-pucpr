74dd7f48286106cc35e02af79fe8c7da51ffb78a
Pedro Henrique Ceciliano <phd.siciliano@gmail.com>
2025-08-15 10:46:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ app = FastAPI()
 async def root():
     return {"message": "API - HelloWorld"}
 # http://127.0.0.1:8000/teste1
-@app.get("/teste1")
+@app.get("/teste")
 async def funcaoteste():
     return {"teste":"deu certo"}
